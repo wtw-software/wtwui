c6963deb96756732f0df0b132b23034c0b85ff45
Andre Tangen @gorillatron <jornandretangen@gmail.com>
2014-02-12 14:51:26 +0100

    browserify

--- main.js
+++ main.js
@@ -1,117 +1,140 @@
-define([
 
-  'wtwui/lib/Dialog',
-  'wtwui/lib/Confirmation',
-  'wtwui/lib/Overlay',
-  'wtwui/lib/Tip',
-  'wtwui/lib/Crit'
-  
-],function(Dialog, Confirmation, Overlay, Tip, Crit) {
-  
-  return {
+var Dialog        = require('./lib/Dialog'),
+    Confirmation  = require('./lib/Confirmation'),
+    Overlay       = require('./lib/Overlay'),
+    Tip           = require('./lib/Tip')
 
-    Dialog: Dialog,
 
-    dialog: function() {
-      var dialog, options
-      if(typeof arguments[0] === 'object') {
-        options = arguments[0]
-      } else {
-        options = {}
-        if(arguments.length === 1) {
-          options.message = arguments[0]
-        }
-        if(arguments.length === 2) {
-          options.title = arguments[0]
-          options.message = arguments[1]
-        } 
-      }
-      var dialog = new Dialog(options)
-      dialog.show()
-      return dialog
-    },
+/*
+ * Dialog
+ * @api public
+*/
+module.exports.Dialog = Dialog
 
-    Confirmation: Confirmation,
+/*
+ * Confirmation
+ * @api public
+*/
+module.exports.Confirmation = Confirmation
 
-    confirm: function() {
-      var self, title, message, callback
-      self = this
-      if(typeof arguments[arguments.length - 1] === 'function') {
-        callback = arguments[arguments.length - 1]
-      }
-      if(arguments.length === 2 && callback) {
-        if(typeof arguments[0] === 'string') {
-          message = arguments[0]
-        }
-      } else if(arguments.length > 2) {
-        title = arguments[0]
-        message = arguments[1]
-      } else if(arguments.length === 3) {
-        
-      }
-      var confirmation = new Confirmation({
-        title: title || null,
-        message: message || 'Are you sure?'
-      })
-      if(callback) {
-        confirmation.ok(function() {
-          callback.call(self, true)
-        })
-        confirmation.cancel(function() {
-          callback.call(self, false)
-        })
-      }
-      confirmation.show()
-      return confirmation
-    },
+/*
+ * Overlay
+ * @api public
+*/
+module.exports.Overlay = Overlay
 
-    Overlay: Overlay,
+/*
+ * Tip
+ * @api public
+*/
+module.exports.Tip = Tip
 
-    overlay: function() {
-      var options, overlay
-      options = {}
-      if(arguments.length === 1) {
-        if(typeof arguments[0] === 'string' || arguments[0] instanceof $) {
-          options.content = arguments[0]
-        } else if(typeof arguments[0] === 'object') {
-          options = arguments[0]
-        } else if(arguments[0].$el && arguments[0].$el instanceof $) {
-          options.content = arguments[0].$el
-        }
-      }
-      overlay = new Overlay(options)
-      overlay.show()
-      return overlay
-    },
 
-    Tip: Tip,
-
-    tip: function() {
-      var options
-      if(typeof arguments[0] === 'object') {
-        options = arguments[0]
-      } else if(typeof arguments[0] === 'string' || arguments[0] instanceof $) {
-        options = {
-          target: arguments[0]
-        }
-        if(typeof arguments[1] === 'string') {
-          options.content = arguments[1]
-        }
-        if(typeof arguments[2] === 'string') {
-          options.position = arguments[2]
-        }
-      }
-      var tip = new Tip(options)
-      tip.visible = true
-      setTimeout(function() {
-        if(tip.$target && tip.visible) {
-          tip.show()
-        }
-      },0)
-      return tip
-    },
-
-    Crit: Crit
+/*
+ * Shorthand for using Dialog
+ * @api public
+*/
+module.exports.dialog = function dialog() {
+  var dialog, options
+  if(typeof arguments[0] === 'object') {
+    options = arguments[0]
+  } else {
+    options = {}
+    if(arguments.length === 1) {
+      options.message = arguments[0]
+    }
+    if(arguments.length === 2) {
+      options.title = arguments[0]
+      options.message = arguments[1]
+    } 
   }
-  
-})
+  var dialog = new Dialog(options)
+  dialog.show()
+  return dialog
+}
+
+/*
+ * Shorthand for using Comfirmation
+ * @api public
+*/
+module.exports.confirm = function confirm() {
+  var self, title, message, callback
+  self = this
+  if(typeof arguments[arguments.length - 1] === 'function') {
+    callback = arguments[arguments.length - 1]
+  }
+  if(arguments.length === 2 && callback) {
+    if(typeof arguments[0] === 'string') {
+      message = arguments[0]
+    }
+  } else if(arguments.length > 2) {
+    title = arguments[0]
+    message = arguments[1]
+  } else if(arguments.length === 3) {
+    
+  }
+  var confirmation = new Confirmation({
+    title: title || null,
+    message: message || 'Are you sure?'
+  })
+  if(callback) {
+    confirmation.ok(function() {
+      callback.call(self, true)
+    })
+    confirmation.cancel(function() {
+      callback.call(self, false)
+    })
+  }
+  confirmation.show()
+  return confirmation
+}
+
+/*
+ * Shorthand for using Overlay
+ * @api public
+*/
+module.exports.overlay = function overlay() {
+  var options, overlay
+  options = {}
+  if(arguments.length === 1) {
+    if(typeof arguments[0] === 'string' || arguments[0] instanceof $) {
+      options.content = arguments[0]
+    } else if(typeof arguments[0] === 'object') {
+      options = arguments[0]
+    } else if(arguments[0].$el && arguments[0].$el instanceof $) {
+      options.content = arguments[0].$el
+    }
+  }
+  overlay = new Overlay(options)
+  overlay.show()
+  return overlay
+}
+
+/*
+ * Shorthand for Tip
+ * @api public
+*/
+module.exports.tip = function tip() {
+  var options
+  if(typeof arguments[0] === 'object') {
+    options = arguments[0]
+  } else if(typeof arguments[0] === 'string' || arguments[0] instanceof $) {
+    options = {
+      target: arguments[0]
+    }
+    if(typeof arguments[1] === 'string') {
+      options.content = arguments[1]
+    }
+    if(typeof arguments[2] === 'string') {
+      options.position = arguments[2]
+    }
+  }
+  var tip = new Tip(options)
+  tip.visible = true
+  setTimeout(function() {
+    if(tip.$target && tip.visible) {
+      tip.show()
+    }
+  },0)
+  return tip
+}
